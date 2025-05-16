951727e924a36f5980eac7ccf8825f6795502fcb
Tushar-designs-arc <sainitushar322@gmail.com>
2025-05-16 23:51:16 +0530

    first commit

---
+++ DSA__Practice__Ques.cpp
@@ -0,0 +1,24 @@
+/* Ques 1: Write a C++ Program to Check Whether a Number is a Positive or Negative Number. */
+
+// #include <iostream>
+// using namespace std;
+    
+// int main() {
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+
+//     if (num != 0)  {
+//         if (num > 0) {
+//             cout << num << " is a positive number.";
+//         }
+//         else {
+//             cout << num << " is a negative number.";
+//         }
+//     }
+//     else {
+//         cout << num << " is neither a positive nor a negative number.";
+//     }
+    
+//     return 0;
+// }
